cc02f227a753500484ae121a4c1dc7b2600b9084
rawadabu <rawadabusaleh0@gmail.com>
2023-04-21 13:19:42 +0300

    171

--- numbers-and-dates/script.js
+++ numbers-and-dates/script.js
@@ -1,5 +1,16 @@
 console.log(23 === 23.0);
 console.log(0.1 + 0.2);
 
+// Conversion
 console.log(+"23");
 console.log(Number("23"));
+
+// Parsing
+console.log(Number.parseInt("30px")); // JS tries to figure out the number that is in this string
+console.log(Number.parseInt("e23")); // Not gonna work, output => NaN
+
+console.log(Number.parseFloat("2.5rem"));
+
+console.log(Number.isNaN(20));
+console.log(Number.isFinite(20));
+console.log(Number.isInteger(20));
