eb86cafe0a7c9e4e5d44b340ea3e676f65896431
rawadabu <rawadabusaleh0@gmail.com>
2023-04-28 12:12:25 +0300

    Math and Rounding

--- bank-project/script.js
+++ bank-project/script.js
@@ -86,7 +86,7 @@ const displayMovements = function (movements, sort = false) {
             <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-            <div class="movements__value">${mov} €</div>
+            <div class="movements__value">${mov.toFixed(2)} €</div>
         </div>
         `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -95,19 +95,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance} EUR`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)} EUR`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -117,7 +117,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -199,7 +199,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
     currentAccount.movements.push(amount);
--- numbers-and-dates/script.js
+++ numbers-and-dates/script.js
@@ -14,3 +14,20 @@ console.log(Number.parseFloat("2.5rem"));
 console.log(Number.isNaN(20));
 console.log(Number.isFinite(20));
 console.log(Number.isInteger(20));
+
+// Rounding integers
+
+console.log(Math.round(23.3)); // Rounds to the nearest integer ->23
+console.log(Math.round(23.9)); // Rounds to the nearest integer ->24
+
+console.log(Math.ceil(23.3)); // Rounds up  ->24
+console.log(Math.ceil(23.9)); // Rounds up  ->24
+
+console.log(Math.floor("23.9")); // Rounds down  ->23
+console.log(Math.floor(-23.9)); // Rounds up  ->-23
+console.log(Math.trunc(-23.9)); // Rounds up  ->-24
+
+// Rounding decimals
+console.log((2.7).toFixed(0)); // toFixed returns a string and not a number
+console.log((2.7).toFixed(3)); // Will add zeroes to get 3 decimal parts ->2.700
+console.log(+(2.7).toFixed(3)); // Will add zeroes to get 3 decimal parts ->2.700 (Number returns)
