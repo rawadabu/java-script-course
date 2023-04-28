42922afd7f77a09e2decc92b9873af50777e279b
rawadabu <rawadabusaleh0@gmail.com>
2023-04-28 13:06:07 +0300

    BigInt

--- numbers-and-dates/script.js
+++ numbers-and-dates/script.js
@@ -43,3 +43,11 @@ console.log(isEven(23)); // False
 // Numeric separators
 const diameter = 287_460_000_000; // We need it to look 287,460,000,000, _ is the solution
 console.log(diameter); // -> The engine basically ignores the underscores
+
+// Working with BigInt
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER); // Same as line 48 output
+console.log(BigInt(123123123123));
+console.log(20n > 15); // True
+console.log(11n / 3n); // 3n
+console.log(20n == "20"); // True
