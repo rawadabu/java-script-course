ee687fecc17a2c576acac62f94279c359c26130f
rawadabu <rawadabusaleh0@gmail.com>
2023-05-07 23:05:15 +0300

    DOM Traversing

--- advanced-dom-bankist/script.js
+++ advanced-dom-bankist/script.js
@@ -96,3 +96,28 @@ document.addEventListener("keydown", function (e) {
 //   this.style.backgroundColor = randomColor(0, 255);
 // });
 // So when clicking on Feature, which its navbar link, all the background (child, root) like nav__links and nav will change its colors, interestring
+
+// DOM TRAVERSING
+// const h1 = document.querySelector("h1");
+// // Going downwards: child
+// console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.children);
+// h1.firstElementChild.style.color = "white";
+// h1.lastElementChild.style.color = "white";
+
+// // Going upwards: parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+// // h1.closest(".header").style.background = "black";
+
+// // Going sideways: siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) el.style.transform = "scale(0.5)";
+// });
