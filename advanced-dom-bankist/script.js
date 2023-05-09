e803137c66a1511737aa5ab1d451a00fe89506f7
rawadabu <rawadabusaleh0@gmail.com>
2023-05-09 10:33:25 +0300

    Tabbed Component

--- advanced-dom-bankist/script.js
+++ advanced-dom-bankist/script.js
@@ -45,6 +45,32 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   }
 });
 
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+// Attach event handler for each tab
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab"); // to prevent getting the span when clicking on numbers (gives span in the console)
+  // console.log(clicked);
+  // Guard clause
+  if (!clicked) return;
+
+  // REMOVE ACTIVE TAB
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+
+  // REMOVE ACTIVE CONTENT
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // Active tab
+  clicked.classList.add("operations__tab--active");
+
+  // Active content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
 const openModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
